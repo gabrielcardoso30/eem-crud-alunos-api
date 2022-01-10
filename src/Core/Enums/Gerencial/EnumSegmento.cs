9a217c536926867f06c225dbd3323abd30133eef
Gabriel Cardoso <57575695+gabrielcardoso30@users.noreply.github.com>
2022-01-10 11:02:17 -0300

    Segmentos de educação (infantil ou médio)

---
+++ src/Core/Enums/Gerencial/EnumSegmento.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Core.Enums.Gerencial
+{
+    public enum EnumSegmento
+    {
+        [Description("Infantil")]
+        Infantil,
+        [Description("Médio")]
+        Medio
+    }
+}
---
+++ src/Core/Queries/Gerencial/Handler/GetSegmentoQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.Enums.Gerencial;
+using Core.Helpers;
+using MediatR;
+
+namespace Core.Queries.Gerencial.Handler
+{
+
+    public class GetSegmentoQueryHandler : IRequestHandler<GetSegmentoQuery, Result<IEnumerable<KeyValuePair<string, string>>>>
+    {
+
+        public GetSegmentoQueryHandler()
+        {
+            
+        }
+
+        public async Task<Result<IEnumerable<KeyValuePair<string, string>>>> Handle(GetSegmentoQuery query, CancellationToken cancellationToken)
+        {
+
+            var result = new Result<IEnumerable<KeyValuePair<string, string>>>();
+            IDictionary<string, string> dic = Enum.GetValues(typeof(EnumSegmento)).Cast<object>().ToDictionary(v => ((Enum)v).ObterDescricaoEnum(), k => ((Enum)k).Valor());
+            result.Value = dic.ToArray();
+            result.Count = dic.Count;
+            result.TableCount = dic.Count;
+
+            return result;
+
+        }
+
+    }
+
+}
